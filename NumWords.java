0d954562425d4713440cc964bf7748b0a961b110
talmaikel <tal.mikel@post.runi.ac.il>
2024-11-13 15:20:44 +0200

    6

--- NumWords.java
+++ NumWords.java
@@ -1,14 +1,11 @@
 // Prints a given number using a hundreds, tens, and units notation.
 public class NumWords {
 	public static void main(String args[]) {
-	 
-		int fullnum = Integer.parseInt(args[0]);
-		int saveNum = fullnum;
-		double ones = (double)fullnum % 10;
-		fullnum = fullnum/10;
-		double tens = (double)fullnum % 10;
-		fullnum = fullnum/10;
-		double hundreds = (double)saveNum % 100;
+	    int fullnum = Integer.parseInt(args[0]);
+		int ones = fullnum % 10;
+		int tens = (fullnum-ones) % 100 /10;
+		fullnum = fullnum/100;
+		double hundreds = (double)fullnum % 10;
 
 		System.out.println((int)hundreds +" hundreds, "+(int) tens+ " tens, and "+ (int) ones+ " ones.");
 
